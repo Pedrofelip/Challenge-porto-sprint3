8ba12cf8ed8b91d6edb0051f25caf94b6e716f2c
Kaue Santana <kauesantana_13@hotmail.com>
2023-11-13 22:49:50 -0300

    return tela cadastro

--- challenge-sprint3/challenge-sprint3/my-app/src/pages/cadastro/Cadastro.tsx
+++ challenge-sprint3/challenge-sprint3/my-app/src/pages/cadastro/Cadastro.tsx
@@ -35,3 +35,36 @@ export const Cadastro = () =>{
         setSenhaCadastro(event.target.value)
     }
 
+    const handleCpf = (event) =>{
+        setCpf(event.target.value)
+    }
+
+    const handleApolice = (event) =>{
+        setApolice(event.target.value)
+    }
+
+    const handlePlacaVeiculo = (event) =>{
+        setPlacaVeiculo(event.target.value)
+    }
+
+    return(
+        <>
+            <Header></Header>
+            <section className="secao_form">
+                <div className="tit_secao_form">
+                    <h1>cadastro</h1>
+                    <hr />
+                </div>
+                <form action="" className="fundo_form_cadastro">
+                    <input type="email" placeholder="E-mail" className="input_form" value={emailCadastro} onChange={handleEmailCadastro}/>
+                    <input type="password" placeholder="Senha" className="input_form" value={senhaCadastro} onChange={handleSenhaCadastro}/>
+                    <input type="text" placeholder="CPF" className="input_form" value={cpf} onChange={handleCpf}/>
+                    <input type="text" placeholder="N da apólice" className="input_form" value={apolice} onChange={handleApolice}/>
+                    <input type="text" placeholder="Placa do veículo" className="input_form" value={placaVeiculo} onChange={handlePlacaVeiculo}/>
+                    <button type="button" className="btn_form" handleSubmit={Cadastrar}>cadastrar</button>
+                </form>
+                <p className="redirect_fomrs">Já possui uma conta? <Link to='/login'>Fazer login</Link></p>
+            </section>
+        </>
+    )
+}
