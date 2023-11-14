46de6dc3c88303fd11f50524d0611e6d9474167d
Pedrofelip <pedrofelipbarros@gmail.com>
2023-11-13 22:56:31 -0300

    projeto front finalizado

--- challenge-sprint3/challenge-sprint3/my-app/src/pages/cadastro/Cadastro.tsx
+++ challenge-sprint3/challenge-sprint3/my-app/src/pages/cadastro/Cadastro.tsx
@@ -13,18 +13,26 @@ export const Cadastro = () =>{
 
 
     function Cadastrar() {
-        const dadosCadastro = {emailCadastro, senhaCadastro, cpf, apolice, placaVeiculo};
+        const dadosCadastro = {
+                                nome: emailCadastro, 
+                                senha: senhaCadastro,
+                                cpf: cpf, 
+                                apolice: apolice, 
+                                placa: placaVeiculo
+                            };
 
-        fetch('urlapi', {
+        console.log(dadosCadastro)
+
+        fetch('http://localhost:3000/cliente', {
             method: 'POST',
-            headers: {"Cpntent-type" : "application/json"},
+            headers: {"Content-type" : "application/json"},
             body: JSON.stringify(dadosCadastro)
         }).then((resp) => resp.json())
         .then((data) => {
             console.log(data)
             console.log('usuario cadastrado')
         })
-        .catch((err) => console.log(err))
+        .catch((error) => console.log(error))
     }
 
     const handleEmailCadastro = (event) =>{
@@ -56,12 +64,12 @@ export const Cadastro = () =>{
                     <hr />
                 </div>
                 <form action="" className="fundo_form_cadastro">
-                    <input type="email" placeholder="E-mail" className="input_form" value={emailCadastro} onChange={handleEmailCadastro}/>
-                    <input type="password" placeholder="Senha" className="input_form" value={senhaCadastro} onChange={handleSenhaCadastro}/>
-                    <input type="text" placeholder="CPF" className="input_form" value={cpf} onChange={handleCpf}/>
+                    <input type="email" placeholder="Nome" className="input_form" value={emailCadastro} onChange={handleEmailCadastro}/>
+                    <input type="password" placeholder="Senha (até 6 digitos)" className="input_form" value={senhaCadastro} onChange={handleSenhaCadastro}/>
+                    <input type="text" placeholder="CPF (11 digitos)" className="input_form" value={cpf} onChange={handleCpf}/>
                     <input type="text" placeholder="N da apólice" className="input_form" value={apolice} onChange={handleApolice}/>
-                    <input type="text" placeholder="Placa do veículo" className="input_form" value={placaVeiculo} onChange={handlePlacaVeiculo}/>
-                    <button type="button" className="btn_form" handleSubmit={Cadastrar}>cadastrar</button>
+                    <input type="text" placeholder="Placa do veículo(modelo aaa-0000 ou aaa-0a00)" className="input_form" value={placaVeiculo} onChange={handlePlacaVeiculo}/>
+                    <button type="button" className="btn_form" onClick={Cadastrar}>cadastrar</button>
                 </form>
                 <p className="redirect_fomrs">Já possui uma conta? <Link to='/login'>Fazer login</Link></p>
             </section>
