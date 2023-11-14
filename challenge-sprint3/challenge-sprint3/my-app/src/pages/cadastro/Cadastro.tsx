ce6f9f532fb45fdac7714d6da36a5ab398fb2677
Kaue Santana <kauesantana_13@hotmail.com>
2023-11-13 22:49:05 -0300

    Tela cadastro quase pronta

--- challenge-sprint3/challenge-sprint3/my-app/src/pages/cadastro/Cadastro.tsx
+++ challenge-sprint3/challenge-sprint3/my-app/src/pages/cadastro/Cadastro.tsx
@@ -12,3 +12,26 @@ export const Cadastro = () =>{
     const[placaVeiculo, setPlacaVeiculo] = useState("")
 
 
+    function Cadastrar() {
+        const dadosCadastro = {emailCadastro, senhaCadastro, cpf, apolice, placaVeiculo};
+
+        fetch('urlapi', {
+            method: 'POST',
+            headers: {"Cpntent-type" : "application/json"},
+            body: JSON.stringify(dadosCadastro)
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            console.log('usuario cadastrado')
+        })
+        .catch((err) => console.log(err))
+    }
+
+    const handleEmailCadastro = (event) =>{
+        setEmailCadastro(event.target.value)
+    }
+
+    const handleSenhaCadastro = (event) =>{
+        setSenhaCadastro(event.target.value)
+    }
+
